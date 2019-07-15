88fefbac4c1a412e4aca12c9ff5aeede0b920ec4
Ellis Dodwell <ellis.dodwell@gmail.com>
2019-07-15 17:52:26 +0100

    update

--- src/plugins/planMonitor.js
+++ src/plugins/planMonitor.js
@@ -1,6 +1,5 @@
 import axios from 'axios'
 
-
 export function getPlanData(plan_id) {
   plan_id = plan_id || 'TOpflW7W09+v72bfNTNNuVIQg7HrM9yHNs5qCwtxESOQg3YZB16Gw5GTRQjuKeTStGGVEIMWRuT2l7QcUI7c44fDhzuoJNrc2mLvX+5Tgy0='
 
@@ -11,6 +10,14 @@ export function getPlanData(plan_id) {
      var win = window.open("", "Title", "resizable=yes,width=780,height=200,top="+(screen.height-400)+",left="+(screen.width-840));
      win.document.body.innerHTML = html;
      win.document.querySelector("form").submit()
+
+     const tableRows = win.document.getElementById('tblQuantities').getElementsByTagName('tr')
+     const headers = ['name_he','approved','change','detailed_plan_total','outline_plan_total','notes']
+     quantities = tableRows.reduce((acc,row)=>{
+       row.children()
+     },[])
+
+
 
      //<script language="javascript">document.redirect_form.submit();</script>
      //toolbar=no,location=no,directories=no,status=no,menubar=no,scrollbars=yes,
