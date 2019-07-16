ed241a0c3ea93ab51dcbc1715ca23aaff712c063
Ellis Dodwell <ellis.dodwell@gmail.com>
2019-07-16 13:01:46 +0100

    update

--- src/plugins/planMonitor.js
+++ src/plugins/planMonitor.js
@@ -4,13 +4,17 @@ import api from '../api'
 export function getPlanData(mavat_id) {
      mavat_id = mavat_id || 'TOpflW7W09+v72bfNTNNuVIQg7HrM9yHNs5qCwtxESOQg3YZB16Gw5GTRQjuKeTStGGVEIMWRuT2l7QcUI7c44fDhzuoJNrc2mLvX+5Tgy0='
      const url = 'https://script.google.com/macros/s/AKfycby0tzlcXaVgv36LRsVKN1NvxkUgo8XCv_3jpHhxSKE_lCkB41Q/exec?mavat_id='
-     axios.get(url + encodeURIComponent(mavat_id))
+     axios.get(url + encodeURIComponent(mavat_id) + 'callback=myfunc')
      .then(function (response) {
        // handle success
-        console.log(JSON.parse(response))
+        console.log(response)
         return response
      })
 
+}
+
+function myfunc(x) {
+     return x
 }
 //https://script.google.com/macros/s/AKfycby0tzlcXaVgv36LRsVKN1NvxkUgo8XCv_3jpHhxSKE_lCkB41Q/exec?mavat_id=TOpflW7W09%2Bv72bfNTNNuVIQg7HrM9yHNs5qCwtxESOQg3YZB16Gw5GTRQjuKeTStGGVEIMWRuT2l7QcUI7c44fDhzuoJNrc2mLvX%2B5Tgy0%3D
 //api.create('plans',response)
