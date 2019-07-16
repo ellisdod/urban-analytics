0cc232d3c0b713baefada99c91baaae66ab084a7
Ellis Dodwell <ellis.dodwell@gmail.com>
2019-07-16 12:15:46 +0100

    update

--- src/plugins/planMonitor.js
+++ src/plugins/planMonitor.js
@@ -3,13 +3,13 @@ import api from '../api'
 
 export function getPlanData(mavat_id) {
      mavat_id = mavat_id || 'TOpflW7W09+v72bfNTNNuVIQg7HrM9yHNs5qCwtxESOQg3YZB16Gw5GTRQjuKeTStGGVEIMWRuT2l7QcUI7c44fDhzuoJNrc2mLvX+5Tgy0='
-     const url = 'https://script.google.com/macros/s/AKfycby0tzlcXaVgv36LRsVKN1NvxkUgo8XCv_3jpHhxSKE_lCkB41Q/exec?mavat_id='
+     const url = 'https://script.google.com/macros/s/AKfycby0tzlcXaVgv36LRsVKN1NvxkUgo8XCv_3jpHhxSKE_lCkB41Q/exec?callback=?mavat_id='
      axios.get(url + encodeURIComponent(mavat_id))
      .then(function (response) {
        // handle success
         console.log(JSON.parse(response))
         return response
-    })
+     })
 
 }
 
