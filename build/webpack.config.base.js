a6046e25fdc7b15c69396315bcfa5ebaa6aaf023
Ellis Dodwell <ellis.dodwell@gmail.com>
2019-12-02 14:21:45 +0000

    update

--- build/webpack.config.base.js
+++ build/webpack.config.base.js
@@ -3,6 +3,7 @@ var nodeExternals = require('webpack-node-externals');
 const HtmlWebpackPlugin = require('html-webpack-plugin')
 const CopyWebpackPlugin = require('copy-webpack-plugin')
 const { VueLoaderPlugin } = require('vue-loader')
+const { CleanWebpackPlugin } = require('clean-webpack-plugin')
 const path = require('path')
 
 const utils = require('./utils')
@@ -88,6 +89,7 @@ module.exports = {
      template: path.join(__dirname, '..', 'index.html'),
       inject: true
     }),
+    new CleanWebpackPlugin(),
     new VueLoaderPlugin(),
     new CopyWebpackPlugin([{
       from: utils.resolve('static/img'),
